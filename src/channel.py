e7b2d063a4fed2362bb1cd51fb1b18b7b0224638
Olesja1509 <olesja.janjic@gmail.com>
2023-06-08 18:52:25 +0200

    edit class Channel

--- src/channel.py
+++ src/channel.py
@@ -13,7 +13,7 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
-    def print_info(self) -> str:
+    def print_info(self):
         """Выводит в консоль информацию о канале."""
 
         channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
